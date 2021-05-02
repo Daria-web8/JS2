57b31b9643125942f90e785472250362b0091e11
Daria B <badrenkovadasha@yandex.ru>
2021-05-02 21:40:09 +0300

    homework

--- js/script.js
+++ js/script.js
@@ -1,40 +1,53 @@
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const API_URL =
+  'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
 
 const app = new Vue({
   el: '#app',
   data: () => ({
     goods: [],
     filteredGoods: [],
-    searchLine: ''
+    searchLine: '',
+    isVisibleCart: false,
   }),
   mounted() {
     this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-      this.goods = JSON.parse(goods);
-      this.filteredGoods = JSON.parse(goods);
-    });
+      this.goods = JSON.parse(goods)
+      this.filteredGoods = JSON.parse(goods)
+    })
   },
   methods: {
+    filterGoods() {
+      console.log('this.searchLine :>> ', this.searchLine)
+      const regexp = new RegExp(this.searchLine, 'i')
+      this.filteredGoods = this.goods.filter((good) =>
+        regexp.test(good.product_name)
+      )
+    },
+    handleCart() {
+      this.isVisibleCart = !this.isVisibleCart
+    },
     makeGETRequest(url, callback) {
-      let xhr;
+      let xhr
+
       if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-      } else if (window.ActiveXObject) { 
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        xhr = new XMLHttpRequest()
+      } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject('Microsoft.XMLHTTP')
       }
 
       xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-          callback(xhr.responseText);
+          callback(xhr.responseText)
         }
       }
 
-      xhr.open('GET', url, true);
-      xhr.send();
-    }
+      xhr.open('GET', url, true)
+      xhr.send()
+    },
   },
   watch: {
     filteredGoods() {
-      console.log('this.filteredGoods :>> ', this.filteredGoods);
-    }
-  }
+      console.log('this.filteredGoods :>> ', this.filteredGoods)
+    },
+  },
 })
